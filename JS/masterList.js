56fdda8b34b9ee39e394e516c50b104ef7ab8ea3
Connor Zhao <connorzhao@10-200-246-21-eduroam.staff.utas.edu.au>
2020-04-05 23:14:37 +1000

    fix bug

--- JS/masterList.js
+++ JS/masterList.js
@@ -1,12 +1,7 @@
-$("#btnCreate").click(function () {
-    alert('2')
-})
 $(function () {
     $('.selectpicker').selectpicker();
 });
 
-
-function edit(params) {
-    console.log(params)
-    console.log(params.parentNode.parentNode.parentNode);
-}
+$('#createNewStaff').on('hidden.bs.modal', function (e) {
+    $("#fCNSta")[0].reset();
+})
