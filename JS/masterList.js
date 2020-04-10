286ea357a0ef11f2eb79d149c26c69781db468d2
Connor Zhao <connorzhao@10-200-246-21-eduroam.staff.utas.edu.au>
2020-04-10 20:01:17 +1000

    managestaff没填东西

--- JS/masterList.js
+++ JS/masterList.js
@@ -1,7 +1,25 @@
-$(function () {
-    $('.selectpicker').selectpicker();
-});
-
 $('#createNewStaff').on('hidden.bs.modal', function (e) {
     $("#fCNSta")[0].reset();
 })
+$("body").on('click', '[data-stopPropagation]', function (e) {
+    e.stopPropagation();
+});
+
+function editStaff(e) {
+
+}
+
+function removeStaff(e) {
+    $(e).parents('tr').remove();
+    console.log($(e).parents('tr').attr('id'));
+}
+
+function validate() {
+    if ($('.checkboxGroup').find("input.custom-control-input:checked").length <= 0) {
+        $('.checkboxGroup').addClass('is-invalid');
+        return false;
+    } else {
+        $('.checkboxGroup').removeClass('is-invalid');
+        return true;
+    }
+}
