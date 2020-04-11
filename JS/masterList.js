186d9e736c46b7769bed52859ab6dfa5481d03fa
Connor Zhao <connorzhao@10-200-246-21-eduroam.staff.utas.edu.au>
2020-04-11 16:20:24 +1000

    save

--- JS/masterList.js
+++ JS/masterList.js
@@ -1,19 +1,43 @@
+//clear form
 $('#createNewStaff').on('hidden.bs.modal', function (e) {
     $("#fCNSta")[0].reset();
 })
-$("body").on('click', '[data-stopPropagation]', function (e) {
-    e.stopPropagation();
-});
+//clear form
+$('#manageStaff').on('hidden.bs.modal', function (e) {
+    $('#fMSta')[0].reset();
+})
 
 function editStaff(e) {
+    $(e).parents('tr').find('td').each(function () {
+        console.log($(this).text().trim());
+    })
+    // console.log($(e).parents('tr').find('td'));
+}
+
+function editUnit(e) {
+    $(e).parents('tr').find('select').each(function () {
+        $(this).removeAttr('disabled')
+    })
+    $(e).addClass('d-none');
+    $(e).parents('tr').find('.btn-success').removeClass('d-none');
+}
+
+function confirmUnit(e) {
+    $(e).parents('tr').find('select').each(function () {
+        $(this).attr('disabled', 'true')
+    })
+    $(e).addClass('d-none');
+    $(e).parents('tr').find('.btn-primary').removeClass('d-none');
 
 }
 
+//remove staff from database and tables
 function removeStaff(e) {
     $(e).parents('tr').remove();
     console.log($(e).parents('tr').attr('id'));
 }
 
+//validate
 function validate() {
     if ($('.checkboxGroup').find("input.custom-control-input:checked").length <= 0) {
         $('.checkboxGroup').addClass('is-invalid');
